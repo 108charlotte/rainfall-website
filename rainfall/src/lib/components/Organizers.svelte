1dc62300ddc473ccecfd9d83d4c055f3ed6875db
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-01-25 14:10:47 -0500

    back to old organizers page

--- rainfall/src/lib/components/Organizers.svelte
+++ rainfall/src/lib/components/Organizers.svelte
@@ -10,17 +10,17 @@
 <div id="organizers">
     <h1>Organizers</h1>
     <div class="raindrops">
-        <ImageCard baseImage={raindrop1} baseImageSize={"90%"} overlayTop={"32%"} overlayShiftX={"-2%"} overlayImageSize={"34%"}>
+        <ImageCard maxWidth={"400px"} baseImage={raindrop1} baseImageSize={"90%"} overlayTop={"32%"} overlayShiftX={"-2%"} overlayImageSize={"34%"}>
             <img src={charlotte} alt="Charlotte at the Rockefeller center in NYC"/>
             <p class="name">Charlotte Woodrum</p>
-            <p class="bio">Aspiring software or ML engineer who enjoys exploring new technologies! </p>
-            <a href="https://www.charlottewoodrum.dev/" target="_blank">Personal website</a>
+            <p class="bio">Aspiring software or ML engineer who enjoys building cool projects! </p>
             <a href="https://www.linkedin.com/in/charlotte-woodrum-557900370/" target="_blank">LinkedIn</a>
         </ImageCard>
-        <ImageCard baseImage={raindrop2} baseImageSize={"100%"} overlayTop={"70%"} overlayShiftX={"-5%"} overlayImageSize={"34%"}>
+        <ImageCard maxWidth={"400px"} baseImage={raindrop2} baseImageSize={"100%"} overlayTop={"70%"} overlayShiftX={"-5%"} overlayImageSize={"34%"}>
             <img src="" alt=""/>
             <p class="name">Shyla Singal</p>
             <p class="bio">A short about me</p>
+            <a href="" target="_blank">LinkedIn</a>
         </ImageCard>
     </div>
 </div>
@@ -45,32 +45,37 @@
 
     .name {
         margin-top: -5px;
-        font-size: 4vw;
+        font-size: 3vw;
+        width: 24vw; 
         color: rgb(0, 30, 57);
         font-weight: 700;
         line-height: 1.1;
+        align-items: center; 
+        justify-content: center; 
+        margin-bottom: 3vh; 
     }
 
     a, .bio {
-        font-size: 2.2vw;
+        font-size: clamp(10px, 2vw, 20px); 
         margin: 0;
         text-align: center;
-        width: 60%; 
     }
 
     .bio {
         margin-top: -30px;
         color: rgb(0, 50, 97);
+        width: 60%; 
     }
 
     a {
         color: rgb(255, 255, 255);
         text-decoration: underline;
-        background: rgb(8, 106, 142);
-        width: 120px;
-        border-radius: 10px;
-        font-size: 2vw;
         display: inline-block;
-        padding: 6px 8px;
+        transition: background 0.2s ease; 
+        width: auto; 
+    }
+
+    a:hover {
+        color: rgb(196, 234, 247);
     }
 </style>
