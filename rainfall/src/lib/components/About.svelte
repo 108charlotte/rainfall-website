1f7a751036747199fa2e14ca9a1a599a27b0ea08
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-01-25 13:42:54 -0500

    added link to athena

--- rainfall/src/lib/components/About.svelte
+++ rainfall/src/lib/components/About.svelte
@@ -2,5 +2,5 @@
     <h1>What is Rainfall? </h1>
     <p>Rainfall is an event happening March 2026 to close the gender gap in tech. 
         For this event, participants will have opportunities to attend workshops, create a unique final project, network with women in STEM careers, and discover the joy of code! 
-        We're affiliated with the <a href="">Athena initiative</a>, a program whose goal is to support underrepresented genders in tech. </p>
+        We're affiliated with the <a href="https://athena.hackclub.com/">Athena initiative</a>, a program whose goal is to support underrepresented genders in tech. </p>
 </div>
